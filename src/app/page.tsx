c393efba0230006c0bbfca36d4656a9da710dc9a
Mario Yonan <myonan@eknengineering.com>
2024-03-15 23:30:32 +0200

    Update ESLint and Prettier configurations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 export default async function Home() {
-  const coffees = await fetch(process.env.GET_COFFEES_URL!).then((res) => res.json());
-  const wantedCoffee = await fetch(`${process.env.GET_COFFEES_URL}?coffeeId=coffee-1`).then((res) => res.json());
+  const coffees = await fetch(process.env.GET_COFFEES_URL!).then((res) =>
+    res.json()
+  );
+  const wantedCoffee = await fetch(
+    `${process.env.GET_COFFEES_URL}?coffeeId=coffee-1`
+  ).then((res) => res.json());
 
   return (
     <main className="mx-4 mt-4">
@@ -8,7 +12,6 @@ export default async function Home() {
       <ul className="space-y-10">
         {coffees.map((coffee: any) => (
           <li key={coffee.id}>
-
             <h2 className="font-bold">{coffee.name}</h2>
             <p>{coffee.description}</p>
             <p>${coffee.price}</p>
