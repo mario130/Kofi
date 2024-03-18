996b6693c33b397fe22ae945f2f7c277bf8cdbb5
Mario Yonan <myonan@eknengineering.com>
2024-03-18 21:49:08 +0200

    feat: add font & theme to tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,13 @@
 export default async function Home() {
-  const coffees = await fetch(process.env.GET_COFFEES_URL!).then((res) =>
-    res.json()
-  );
-  const wantedCoffee = await fetch(
-    `${process.env.GET_COFFEES_URL}?coffeeId=coffee-1`
-  ).then((res) => res.json());
-
   return (
-    <main className="mx-4 mt-4">
-      <h1 className="text-4xl mb-8">My Coffees</h1>
-      <ul className="space-y-10">
-        {coffees.map((coffee: any) => (
-          <li key={coffee.id}>
-            <h2 className="font-bold">{coffee.name}</h2>
-            <p>{coffee.description}</p>
-            <p>${coffee.price}</p>
-          </li>
-        ))}
-      </ul>
-
-      <h2 className="text-2xl mt-8">Wanted Coffee</h2>
-      <p>{wantedCoffee.name}</p>
+    <main className="">
+      <div className="bg-secondary p-4 text-t-green">Hello there</div>
+      <h1>Heyy</h1>
+      <h2>Heyy</h2>
+      <h3>Heyy</h3>
+      <h4>Heyy</h4>
+      <p>Heyy</p>
+      <span>Heyy</span>
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,13 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: "#0E8784",
+        secondary: "#FDD6BA",
+        "t-orange": "#E94F37",
+        "t-gray": "#83888F",
+        "t-cream": "#FEFCF7",
+        "t-dark": "#262626",
       },
     },
   },
