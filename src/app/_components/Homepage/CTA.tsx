a145e89cebdc7fdc3c7a894031a595d6b34a6d83
Mario Yonan <myonan@eknengineering.com>
2024-03-19 05:47:27 +0200

    fix: CTA overflow on mobile

--- src/app/_components/Homepage/CTA.tsx
+++ src/app/_components/Homepage/CTA.tsx
@@ -15,8 +15,8 @@ export default function CTA() {
           layout="responsive"
           objectFit="cover" />
       </picture>
-      <div className="absolute top-0 p-4 text-center md:text-left text-white h-full flex flex-col justify-center">
-        <div className="h-96 2xl:h-80 flex flex-col justify-between items-center md:items-start md:pl-8">
+      <div className="absolute top-0 text-center md:text-left text-white h-full flex flex-col justify-center">
+        <div className="h-full py-10 sm:py-0 sm:h-96 2xl:h-80 flex flex-col justify-between items-center md:items-start md:pl-8">
           <h1 className="text-6xl font-bold md:w-[400px] xl:w-[700px] 2xl:w-[1000px]">Great coffee made simple.</h1>
           <p className="text-lg font-extralight mt-5 leading-8 w-96 md:w-[450px] xl:w-[600px]">Start your mornings with the world’s best coffees. Try our expertly curated artisan
             coffees from our best roasters delivered directly to your door, at your schedule.</p>
