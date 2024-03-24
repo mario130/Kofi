4b5a5a33a8b37a79fd76cb002065a4b7f6f87bc9
Mario Yonan <myonan@eknengineering.com>
2024-03-24 23:40:08 +0200

    feat: add error page

---
+++ src/app/error.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import { Button } from "./_components/ui/button";
+
+export default function ErrorPage() {
+
+  return (
+    <main className="max-w-7xl xl:mx-auto px-6 py-10 text-center">
+      <h3 className="mb-4">Oops! Something went wrong...</h3>
+      <p>Sorry, it seems that there was an error.</p>
+      <p>But don't worry, we're brewing up a solution!</p>
+      <Button className="mt-6" onClick={() => window.location.reload()}>Refresh the page</Button>
+    </main>
+  );
+};
