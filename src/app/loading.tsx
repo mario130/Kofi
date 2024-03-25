4fc2fb47ac221a04862fa134172b5232809341b2
Mario Yonan <myonan@eknengineering.com>
2024-03-25 02:32:30 +0200

    fix: remove spinners as not used

--- src/app/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { PropagateLoader } from "react-spinners";
-
-export default function Loading() {
-  return (
-    <div className="flex max-w-7xl justify-center px-6 py-10 xl:mx-auto">
-      <PropagateLoader color="#e9e3dd90" loading />
-    </div>
-  );
-}
